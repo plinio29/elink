b3d00664776590c96b3adc88868cc5784fee5d9f
Plínio Ramos <plinio206@live.com>
2024-01-18 20:30:58 -0300

    Organização dos diretórios do site.

--- script/imagem.js
+++ script/imagem.js
@@ -16,22 +16,22 @@ btesquerdo.addEventListener('click', alteracard2);
 function alteracard1(){
     switch(numero){
         case 1:
-            card.src = 'imagens/logo/elink2.jpg'
-            link.href = 'pagina2.html'
+            card.src = 'imagens/modelos/elink2.jpg'
+            link.href = '/modelos/pagina2.html'
             numero = numero + 2
             break
         case 3:
-            card.src = 'imagens/logo/elink3.jpg'
-            link.href = 'pagina4.html'
+            card.src = 'imagens/modelos/elink3.jpg'
+            link.href = '/modelos/pagina4.html'
             numero = numero + 1
             break
         case 4:
-            card.src = 'imagens/logo/elink4.jpg'
-            link.href = 'pagina3.html'
+            card.src = 'imagens/modelos/elink4.jpg'
+            link.href = '/modelos/pagina3.html'
             break
         default:
-            card.src = 'imagens/logo/elink1.jpg'
-            link.href = 'pagina1.html'
+            card.src = 'imagens/modelos/elink1.jpg'
+            link.href = '/modelos/pagina1.html'
             break
     }
 }
@@ -40,22 +40,22 @@ function alteracard1(){
 function alteracard2(){
     switch(numero){
         case 4:
-            card.src = 'imagens/logo/elink3.jpg'
-            link.href = 'pagina4.html'
+            card.src = 'imagens/modelos/elink3.jpg'
+            link.href = '/modelos/pagina4.html'
             numero = numero - 1
             break
         case 3:
-            card.src = 'imagens/logo/elink2.jpg'
-            link.href = 'pagina2.html'
+            card.src = 'imagens/modelos/elink2.jpg'
+            link.href = '/modelos/pagina2.html'
             numero = numero - 2
             break
         case 1:
-            card.src = 'imagens/logo/elink1.jpg'
-            link.href = 'pagina1.html'
+            card.src = 'imagens/modelos/elink1.jpg'
+            link.href = '/modelos/pagina1.html'
             break
         default:
-            card.src = 'imagens/logo/elink1.jpg'
-            link.href = 'pagina1.html'
+            card.src = 'imagens/modelos/elink1.jpg'
+            link.href = '/modelos/pagina1.html'
             break
     }
 }
